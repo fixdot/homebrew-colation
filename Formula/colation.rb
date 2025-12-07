d421588a51254c8bdb403c881a60611db8974656
fixdot <fixdot@gmail.com>
2025-12-07 17:55:35 +0900

    Create placeholder formula for Colation project

---
+++ Formula/colation.rb
@@ -0,0 +1,11 @@
+class Colation < Formula
+  desc "Placeholder formula for the :Colation project"
+  homepage "https://colation.codes"
+  url "https://example.com/colation-0.0.0.tar.gz"
+  version "0.0.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  def install
+    ohai "This is a placeholder formula. The actual :Colation CLI will be provided in a future release."
+  end
+end
